0f24de7dab865489b634ce69ce5d40877e485d9e
Clarence Williams <xwilliams.clarencex@gmail.com>
2020-12-02 22:09:07 +0000

    w2d3

--- play.js
+++ play.js
@@ -7,3 +7,24 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', data => {
+    handleUserInput(data);
+  });
+  return stdin;
+};
+
+setupInput();
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+handleUserInput();
